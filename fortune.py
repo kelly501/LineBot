d7a78b825cfeea1b36ce5f0b3df6b3af8bbf6536
Kaze <82431652+kelly501@users.noreply.github.com>
2022-06-15 16:51:17 +0800

    Update fortune.py

--- fortune.py
+++ fortune.py
@@ -3,7 +3,7 @@ import random
 from linebot.models import TextSendMessage
 import os
 
-line_uuid = os.environ['UUID']
+line_uuid = os.environ['LINE_UUID']
 token = os.environ['TOKEN']
 fortune = random.choice(['大凶', '凶', '末吉', '吉','中吉','大吉'])
 line_bot_api = LineBotApi(token)
