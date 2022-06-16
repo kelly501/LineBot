b81828e1f0cc80fce767b9155d43b6f788b197d3
Kaze <82431652+kelly501@users.noreply.github.com>
2022-06-16 22:14:03 +0800

    Update fortune.py

--- fortune.py
+++ fortune.py
@@ -3,15 +3,6 @@ import random
 from linebot.models import TextSendMessage
 import os
 
-version: 2
-updates:
-  - package-ecosystem: "npm"
-    directory: "/"
-    schedule:
-      interval: "daily"
-      time: "09:00"
-      # Use Japan Standard Time (UTC +09:00)
-      timezone: "Asia/Taipei"
 
 line_uuid = os.environ['LINE_UUID']
 token = os.environ['TOKEN']
