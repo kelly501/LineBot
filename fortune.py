ca8a211913f83440a9531aab176e30a6d7d461e8
Kaze <82431652+kelly501@users.noreply.github.com>
2022-06-16 22:10:37 +0800

    Update fortune.py

--- fortune.py
+++ fortune.py
@@ -3,6 +3,16 @@ import random
 from linebot.models import TextSendMessage
 import os
 
+version: 2
+updates:
+  - package-ecosystem: "npm"
+    directory: "/"
+    schedule:
+      interval: "daily"
+      time: "09:00"
+      # Use Japan Standard Time (UTC +09:00)
+      timezone: "Asia/Taipei"
+
 line_uuid = os.environ['LINE_UUID']
 token = os.environ['TOKEN']
 fortune = random.choice(['大凶', '凶', '末吉', '吉','中吉','大吉'])
@@ -11,3 +21,4 @@ line_bot_api = LineBotApi(token)
 line_bot_api.push_message(
     line_uuid,
     TextSendMessage(text=fortune))
+
